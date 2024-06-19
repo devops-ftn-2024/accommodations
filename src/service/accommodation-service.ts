45e0b7d7dce95e6a56b4ed319bcc509845620c0f
Aleksandra Nedic <nedicaleksandra843@gmail.com>
2024-06-19 22:02:10 +0200

    chore: add loging metrics and tracing

--- src/service/accommodation-service.ts
+++ src/service/accommodation-service.ts
@@ -3,6 +3,7 @@ import { AccommodationRepository } from "../repository/accommodation-repository"
 import { Accommodation, AccommodationInput } from "../types/accommodation";
 import { BadRequestError, ForbiddenError, InternalServerError } from "../types/errors";
 import { LoggedUser, Role, UsernameDTO } from "../types/user";
+import { Logger } from "../util/logger";
 import { validateAccommodationInput } from "../util/validation";
 
 
@@ -17,16 +18,20 @@ export class AccommodationService {
 
     async getAccommodation(id: string) {
         if (!id) {
+            Logger.error("BadRequestError: Missing id parameter");
             throw new BadRequestError("Missing id parameter");
         }
         return this.repository.getAccommodation(id);
     }
 
     async createAccommodation(loggedUser: LoggedUser, accommodationInput: AccommodationInput) {
+        Logger.log('Creating new accommodation');
         if (loggedUser?.role !== Role.HOST) {
+            Logger.error("ForbiddenError: Only hosts can create accommodations");
             throw new ForbiddenError("Only hosts can create accommodations");
         }
         if (!loggedUser?.username) {
+            Logger.error("BadRequestError: Missing logged user username parameter");
             throw new BadRequestError("Missing logged user username parameter");
         }
         validateAccommodationInput(accommodationInput);
@@ -37,6 +42,7 @@ export class AccommodationService {
             rating: 0,
         };
         const accommodationId = await this.repository.createAccommodation(accommodation);
+        Logger.log(`New accommodation created with id: ${accommodationId}`);
         try {
             const accommodationData = {
                 accommodationId,
@@ -48,6 +54,7 @@ export class AccommodationService {
                 maxCapacity: accommodation.maxCapacity,
 
             }
+            Logger.log(`Emitting accommodation-created event: ${JSON.stringify(accommodationData)}`);
             this.eventQueue.execute(accommodationData, 'accommodation-created');
             return {
                 _id: accommodationId,
@@ -60,17 +67,22 @@ export class AccommodationService {
     }
 
     async getAccommodationByUser(user: LoggedUser) {
+        Logger.log(`Getting all accommodations which belongs to user: ${JSON.stringify(user)}`);
         if (!user?.username) {
+            Logger.error("BadRequestError: Missing username parameter");
             throw new BadRequestError("Missing username parameter");
         }
         if (user.role !== Role.HOST) {
+            Logger.error("ForbiddenError: Only hosts can get accommodations by user");
             throw new ForbiddenError("Only hosts can get accommodations by user");
         }
         return this.repository.getAccommodationByUser(user.username);
     }
 
     async updateUsername(usernameDTO: UsernameDTO) {
+        Logger.log(`Updating username from ${usernameDTO.oldUsername} to ${usernameDTO.newUsername}`);
         if (!usernameDTO?.oldUsername || !usernameDTO?.newUsername) {
+            Logger.error("BadRequestError: Missing username parameter");
             throw new BadRequestError("Missing username parameter");
         }
         return this.repository.updateUsername(usernameDTO);
